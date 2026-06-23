623e177f13d863abd46f17cc29507eb06b5f9e2d
Helder Daiha <helderdaiha@MacBook-Pro-de-Helder.local>
2026-06-23 12:27:03 -0300

    modificações-dash

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
 
-st.set_page_config(page_title="Igreja do Nazareno VIVA", layout="wide")
-st.title("Igreja do Nazareno VIVA")
+pg = st.navigation([
+    st.Page("pages/1_Dados_dos_encontros.py", title="Analise dos Grupos de Crescimento"),
+    st.Page("pages/2_Novos_decididos.py", title="Novos Decididos"),
+])
 
-st.write("Use a navegação para explorar as diferentes páginas.")
-
+pg.run()
 
 
