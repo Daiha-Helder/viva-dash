c45c3d06f46e1fe194e2fa045e336188e93078ae
Helder Daiha <helderdaiha@MacBook-Pro-de-Helder.local>
2026-07-04 17:17:31 -0300

    first-fix

--- pages/1_dados_do_culto.py
+++ pages/1_dados_do_culto.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 from src.data_preparation import gera_columns, prepara_dados_batismo, concatenando_dados, resultados_anuais
 from src.gera_figuras import gera_figura, gera_figura_atributo, gera_figura_batismo, gera_figura_media, prepara_dados_anuais_plot, prepara_dados_mensais_plot, ComparaDadosAnuais
 
