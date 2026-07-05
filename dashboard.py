53259905eec11896467ed3ffe69ad7b233473c0d
Helder Daiha <helderdaiha@MacBook-Pro-de-Helder.local>
2026-07-04 21:27:07 -0300

    fix

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ st.set_page_config(
 
 pg = st.navigation([
     st.Page("pages/1_dados_do_culto.py", title="Dados do culto"),
-    st.Page("pages/2_novos_decididos.py", title="Novos Decididos"),
+    st.Page("pages/2_novos_decididos.py", title="Novos Decididos")
 ])
 
 pg.run()
