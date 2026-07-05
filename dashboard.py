2906a6ab17737e11ef8fc22f62d4570860632b41
Helder Daiha <helderdaiha@MacBook-Pro-de-Helder.local>
2026-07-04 21:20:20 -0300

    trird-fix

--- dashboard.py
+++ dashboard.py
@@ -1,14 +1,13 @@
 import streamlit as st
-from pathlib import Path
 
 st.set_page_config(
     layout="wide"
 )
 
-base_dir = Path(__file__).resolve().parent
+
 pg = st.navigation([
-    st.Page(str(base_dir/"pages"/"1_dados_do_culto.py"), title="Dados do culto"),
-    st.Page(str(base_dir/"pages"/"2_novos_decididos.py"), title="Novos Decididos"),
+    st.Page("pages/1_dados_do_culto.py", title="Dados do culto"),
+    st.Page("pages/2_novos_decididos.py", title="Novos Decididos"),
 ])
 
 pg.run()
--- pages/1_dados_do_culto.py
+++ pages/1_dados_do_culto.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 import sys
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
 from src.data_preparation import gera_columns, prepara_dados_batismo, concatenando_dados, resultados_anuais
 from src.gera_figuras import gera_figura, gera_figura_atributo, gera_figura_batismo, gera_figura_media, prepara_dados_anuais_plot, prepara_dados_mensais_plot, ComparaDadosAnuais
 
